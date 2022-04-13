f4e4ed3a8ddf5481827d45a21ed96316eca0056e
XenoneX <amb@bard.su>
2022-04-13 14:03:26 +0300

    Initial commit

---
+++ LinesAndBoxes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinesAndBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
